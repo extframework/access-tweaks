cf1fcf5b9c7312af7985d43cc0afed9d87c5725d
durganmcbroom <durganmcbroom@gmail.com>
2025-07-05 20:37:28 -0700

    Fixed compilation issues.

--- src/gradle/kotlin/com/kaolinmc/extension/access/AccessGradleEntrypoint.kt
+++ src/gradle/kotlin/com/kaolinmc/extension/access/AccessGradleEntrypoint.kt
@@ -1,12 +1,12 @@
 package com.kaolinmc.extension.access
 
-import com.kaolinmc.gradle.api.ExtframeworkExtension
-import com.kaolinmc.gradle.api.GradleEntrypoint
+import com.kaolinmc.kiln.api.KaolinExtension
+import com.kaolinmc.kiln.api.GradleEntrypoint
 import com.kaolinmc.minecraft.MinecraftGradleEntrypoint
 
 class AccessGradleEntrypoint : GradleEntrypoint {
     override suspend fun configure(
-        extension: ExtframeworkExtension,
+        extension: KaolinExtension,
         helper: GradleEntrypoint.Helper
     ) {
         for (environment in extension.environments) {
